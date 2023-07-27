1b934f5bd7ccc7a37258ffe54a8f50a6865175b2
istiaqueahmedarik <istiaqueahmedarik@gmail.com>
2023-07-27 17:58:16 +0600

    :3

--- main.cpp
+++ main.cpp
@@ -100,11 +100,8 @@ if (lp[j] == 0)lp[j] = i;}}}for (int i = 2; i <= n; i++){if (sieve[i]){primes.pu
  */
 // logn fobonacci
 /*
-vector<vector<int>> identityMatrix({{1, 0},{0, 1}});vector<vector<int>> result({{0, 0},{0, 0}});void multiply(vector<vector<int>> a, vector<vector<int>> b){for(auto &i : result){for(auto &j : i){
-j = 0;}} for(int i = 0; i < 2; i++){for(int j = 0; j < 2; j++){for(int k = 0; k < 2; k++){result[i][k] += a[i][j] * b[j][k];
-result[i][k] %= MOD;}}}}void matrixExpo(vector<vector<int>> matrix, int n){if(n == 0){result = identityMatrix;return;}    matrixExpo(matrix, n / 2);multiply(result, result);if(n & 1){multiply(result, matrix);}} int nthFibonacciNumber(int n){if(n <= 1) return n;vector<vector<int>> baseMatrix({{1, 1},{1, 0}});multiply(baseMatrix, identityMatrix);matrixExpo(baseMatrix, n - 1);return result[0][0];}
+vector<vector<int>> identityMatrix({{1, 0},{0, 1}});vector<vector<int>> result({{0, 0},{0, 0}});void multiply(vector<vector<int>> a, vector<vector<int>> b){for(auto &i : result){for(auto &j : i){j = 0;}} for(int i = 0; i < 2; i++){for(int j = 0; j < 2; j++){for(int k = 0; k < 2; k++){result[i][k] += a[i][j] * b[j][k];result[i][k] %= MOD;}}}}void matrixExpo(vector<vector<int>> matrix, int n){if(n == 0){result = identityMatrix;return;}matrixExpo(matrix, n / 2);multiply(result, result);if(n & 1){multiply(result, matrix);}} int nthFibonacciNumber(int n){if(n <= 1) return n;vector<vector<int>> baseMatrix({{1, 1},{1, 0}});multiply(baseMatrix, identityMatrix);matrixExpo(baseMatrix, n - 1);return result[0][0];}
 */
-
 void solve()
 {
 }
