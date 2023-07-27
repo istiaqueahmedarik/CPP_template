69d3fdea77bd3c8cf77721f5e8c253b48ae69d51
istiaqueahmedarik <istiaqueahmedarik@gmail.com>
2023-07-27 18:01:38 +0600

    :3

--- main.cpp
+++ main.cpp
@@ -98,7 +98,7 @@ void _print(T t, V... v)
 /*int bigPow(int x, int n) { int res = 1;while (n){if (n & 1)res *= x;x *= x;n >>= 1;}return res;} vector<int> primes,lp(1000000, 0), hp(1000000, 0);vector<bool> sieve(1000000, true); void generatePrimes(int n) {sieve[0] = sieve[1] = false;for(int i = 2; i * i <= n; i++){if (sieve[i]){lp[i] = hp[i] = i;for (int j = i * i; j <= n; j += i){sieve[j] = false;hp[j] = i;
 if (lp[j] == 0)lp[j] = i;}}}for (int i = 2; i <= n; i++){if (sieve[i]){primes.push_back(i);}}}map<int,int> prime_factor(int x){//generate prime firstmap<int, int> mp;while (x > 1){int prime_factor = hp[x];while (x % prime_factor == 0){mp[prime_factor]++;x /= prime_factor;}}return mp;}}
  */
-// logn fobonacci
+// logn fibonacci
 /*
 vector<vector<int>> identityMatrix({{1, 0},{0, 1}});vector<vector<int>> result({{0, 0},{0, 0}});void multiply(vector<vector<int>> a, vector<vector<int>> b){for(auto &i : result){for(auto &j : i){j = 0;}} for(int i = 0; i < 2; i++){for(int j = 0; j < 2; j++){for(int k = 0; k < 2; k++){result[i][k] += a[i][j] * b[j][k];result[i][k] %= MOD;}}}}void matrixExpo(vector<vector<int>> matrix, int n){if(n == 0){result = identityMatrix;return;}matrixExpo(matrix, n / 2);multiply(result, result);if(n & 1){multiply(result, matrix);}} int nthFibonacciNumber(int n){if(n <= 1) return n;vector<vector<int>> baseMatrix({{1, 1},{1, 0}});multiply(baseMatrix, identityMatrix);matrixExpo(baseMatrix, n - 1);return result[0][0];}
 */
